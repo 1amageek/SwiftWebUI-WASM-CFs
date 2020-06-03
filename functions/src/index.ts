accdfe26ec517914cbe4431573c93487d199f294
1amageek <tmy0x3@icloud.com>
2020-06-03 19:43:32 +0900

    Fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,10 +7,4 @@ const app = express()
 app.use('/scripts', express.static(__dirname + '/scripts'));
 app.use('/wasm', express.static(__dirname + '/wasm'));
 
-console.log(__dirname)
-
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-export const api = functions.https.onRequest(app)
+export const resource = functions.https.onRequest(app)
